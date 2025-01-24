1454c66c97d3f2e511202d5a574fbd3755ba28b4
Tayrika <rika13tay@gmail.com>
2025-01-24 23:53:21 +0900

    Add header title and swiper feature with animal images

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,39 @@
 import React from "react";
+import dog from "../assets/header/dog.png";
+import cat from "../assets/header/cat.png";
+import hamster from "../assets/header/hamster.png";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import { Navigation, Pagination, Autoplay } from "swiper/modules";
 
 const Header = () => {
   return (
-    <header className="w-full h-[70vh] bg-gray-800 text-white flex items-center justify-center">
-      <div className="w-full">
-        <h1 className="text-5xl text-center">The Animal Shelter App</h1>
+    <header className="header w-full h-[70vh] bg-green-200 text-black flex items-center justify-center">
+      <div className="header-content w-1/2 flex flex-col justify-center pl-12 space-y-4 ml-52">
+        <h1 className="text-8xl font-bold text-left mb-1">Find Your Favorite Friend</h1>
+        <p className="text-xl">Explore thousands of furry friends from 10,000+ shelters and rescues waiting for a home.</p>
+      </div>
+      <div className="w-1/2 h-full">
+        <Swiper
+          modules={[Navigation, Pagination, Autoplay]}
+          navigation
+          pagination={{ clickable: true }}
+          autoplay={{ delay: 4000 }}
+          loop
+          className="h-full custom-swiper"
+        >
+          <SwiperSlide>
+            <img src={cat} alt="cat" className="object-fill w-fit h-full"/>
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={dog} alt="dog" className="object-fill w-fit h-full"/>
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={hamster} alt="dog" className="object-fill w-fit h-full"/>
+          </SwiperSlide>
+        </Swiper>
       </div>
     </header>
   );
