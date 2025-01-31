782cb466fa6ca5f9e39696a21daf6551a7621360
Valeriusdev <kakao757@gmail.com>
2025-01-30 23:35:18 -0800

    implement routing and add Menu component

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -5,13 +5,14 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import { Navigation, Pagination, Autoplay } from "swiper/modules";
+import Menu from "./Menu";
 
 const Header = () => {
   return (
-    <header className="header w-full h-[70vh] bg-green-200 text-black flex flex-col md:flex-row items-center justify-center overflow-hidden">
-
+    <header className="header w-full h-[70vh] bg-green-200 text-black flex flex-col md:flex-row items-center justify-center overflow-hidden relative">
       <div className="absolute top-0 left-0 h-[70vh] w-[50%] bg-green-100 z-0 md:-mr-4"></div>
       <div className="absolute top-0 right-0 h-[70vh] w-[46%] bg-green-300 z-0 md:-mr-4"></div>
+      <Menu className="absolute top-7 left-4" />
 
       <div className="header-content w-full md:w-1/2 flex flex-col justify-center items-center p-4 md:pl-12 text-center md:text-left space-y-4 md:ml-52 z-10">
         <h1 className="text-6xl sm:text-7xl md:text-8xl lg:text-8xl font-bold text-center md:text-left lg:text-left mb-1 leading-tight">
