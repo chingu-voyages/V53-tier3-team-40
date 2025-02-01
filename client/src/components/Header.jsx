2c487ba400fbcc23322f4e796851d1095b09bc71
ss-deep <73766624+ss-deep@users.noreply.github.com>
2025-02-01 17:13:16 -0600

    styling

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -9,30 +9,36 @@ import { Navigation, Pagination, Autoplay } from "swiper/modules";
 const Header = () => {
   return (
     <header className="header w-full h-[70vh] bg-green-200 text-black flex flex-col md:flex-row items-center justify-center overflow-hidden">
-
       <div className="absolute top-0 left-0 h-[70vh] w-[50%] bg-green-100 z-0 md:-mr-4"></div>
       <div className="absolute top-0 right-0 h-[70vh] w-[46%] bg-green-300 z-0 md:-mr-4"></div>
 
-      <div className="header-content w-full md:w-1/2 flex flex-col justify-center items-center p-4 md:pl-12 text-center md:text-left space-y-4 md:ml-52 z-10">
-        <h1 className="text-6xl sm:text-7xl md:text-8xl lg:text-8xl font-bold text-center md:text-left lg:text-left mb-1 leading-tight">
-          Find Your Favorite<br />Friend
+      <div className="header-content w-full md:w-1/2 flex flex-col justify-center items-center text-center md:text-left space-y-4 md:ml-48 md:m-16 z-10">
+        <h1 className="text-6xl sm:text-6xl md:text-7xl lg:text-8xl font-bold text-center md:text-left lg:text-left mb-1 leading-tight">
+          Find Your Favorite
+          <br />
+          Friend
         </h1>
         <p className="text-lg md:text-xl md:px-0 max-w-[90%] md:max-w-full">
-          Explore thousands of furry friends from 10,000+ shelters and rescues waiting for a home.
+          Explore thousands of furry friends from 10,000+ shelters and rescues
+          waiting for a home.
         </p>
       </div>
-      <div className="relative w-full md:w-1/2 h-[50vh] md:h-full hidden md:flex items-center justify-center z-10">
+      <div className="relative w-full md:w-1/2 h-[50vh] md:h-full hidden md:flex items-center justify-center z-10 m-4">
         <Swiper
           modules={[Navigation, Pagination, Autoplay]}
           // navigation
           pagination={{ clickable: true }}
           autoplay={{ delay: 4000 }}
           loop
-          className="h-full custom-swiper"
+          className="h-full w-full"
         >
           {animals.map((animal) => (
             <SwiperSlide key={animal._id}>
-              <img src={animal.image} alt={animal.name} className="object-fill w-fit h-full pl-15 pr-20"/>
+              <img
+                src={animal.image}
+                alt={animal.name}
+                className="  sm:h-[85%] sm:w-[90%] md:w-full md:h-full p-4 object-contain"
+              />
             </SwiperSlide>
           ))}
         </Swiper>
