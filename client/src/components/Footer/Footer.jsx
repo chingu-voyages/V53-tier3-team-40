65cd3300878d7f1cb4cd44f8327988946ad112b6
r1g023 <limon1285@gmail.com>
2025-02-07 00:00:25 -0600

    Added footer structure, images, and updated tailwind.config.js

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,11 +1,21 @@
-const Footer = () => {
+import FooterContent from "./FooterContent";
+import FooterBottom from "./FooterBottom";
+import FooterHeader from "./FooterHeader";
+
+function Footer() {
   return (
-    <footer className="w-full h-[40vh] bg-gray-800 text-white flex items-center justify-center">
-      <div className="w-full">
-        <h2 className="text-5xl text-center">Footer</h2>
+    <footer
+      id="footer"
+      className="w-full bg-gray-800 text-teal-400 dark:bg-footerBackgroundColor dark:text-footerTextColor">
+      <div className="max-w-7xl mx-auto rounded-lg col-span-12 pt-10">
+        <div className="w-full mx-auto px-8">
+          <FooterHeader />
+          <FooterContent />
+          <FooterBottom />
+        </div>
       </div>
     </footer>
   );
-};
+}
 
 export default Footer;
---
+++ client/src/components/Footer/FooterTechStack.jsx
@@ -0,0 +1,49 @@
+import PropTypes from "prop-types";
+
+function FooterTechStack({ techstack }) {
+  return (
+    <div className="box2 px-6 py-6 md:px-8 md:py-8 w-full lg:w-2/3 bg-white border border-footerBoxColor flex flex-col items-center rounded-3xl">
+      <h2 className="text-2xl font-bold mb-6 text-gray-800 dark:text-white">
+        Tech Stack
+      </h2>
+
+      <div className="grid grid-cols-2 gap-4 w-full">
+        {techstack.map((tech, index) => (
+          <div
+            key={index}
+            className={`flex justify-center items-center px-4 ${
+              tech.name === "tailwindcss" ? "col-span-2 w-full mt-6" : ""
+            }`}>
+            <a
+              href={tech.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="group">
+              <img
+                src={tech.image}
+                alt={tech.name}
+                className={`object-contain ${
+                  tech.name === "tailwindcss"
+                    ? "w-[90%] md:w-full"
+                    : "w-24 xl:w-32 h-24 xl:h-32"
+                }`}
+              />
+            </a>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+FooterTechStack.propTypes = {
+  techstack: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      image: PropTypes.string.isRequired,
+      url: PropTypes.string
+    })
+  ).isRequired
+};
+
+export default FooterTechStack;
