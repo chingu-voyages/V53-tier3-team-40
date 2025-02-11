e9e6cf00a3073660c76ea3692f6fd69ebd87d4db
Julie Cheng <jucheng925@gmail.com>
2025-02-11 03:52:52 -0500

    cube effect tested

--- client/src/components/Problem2.jsx
+++ client/src/components/Problem2.jsx
@@ -4,55 +4,51 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 
 // Import Swiper styles
 import 'swiper/css';
-import 'swiper/css/effect-flip';
+import 'swiper/css/effect-cube';
 import 'swiper/css/pagination';
-import 'swiper/css/navigation';
-
 
 // import required modules
-import { EffectFlip, Pagination, Navigation } from 'swiper/modules';
-
+import { EffectCube, Pagination } from 'swiper/modules';
 
 const Problem2 = () => {
   return (
+    <>
     <Swiper
-        effect={'flip'}
+        effect={'cube'}
         grabCursor={true}
-        pagination={{ clickable: true }}
-        navigation={true}
-        modules={[EffectFlip, Pagination, Navigation]}
-        className="w-full max-w-7xl h-96"
+        cubeEffect={{
+          shadow: true,
+          slideShadows: true,
+          shadowOffset: 20,
+          shadowScale: 0.94,
+        }}
+        pagination={true}
+        modules={[EffectCube, Pagination]}
+        className="swiper w-full max-w-7xl h-96"
       >
-        <SwiperSlide className='relative'>
-          <p className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center text-white text-xl font-bold" >Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis neque sint eligendi iusto quidem ipsum officia? Optio omnis atque velit porro, distinctio autem, quasi commodi, facilis aperiam nostrum voluptate perspiciatis.</p>
-          <img src="https://swiperjs.com/demos/images/nature-1.jpg" className="w-full h-full object-cover"/>
+        <SwiperSlide className="swiper-slide relative w-full h-full px-[150px]">
+          <img src="https://swiperjs.com/demos/images/nature-1.jpg" className="w-full h-full object-cover" />
+          <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 text-white text-xl font-bold">
+          Dog Slide
+        </div>
         </SwiperSlide>
-        <SwiperSlide className='relative'>
-        <p className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center text-white text-xl font-bold" >Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis neque sint eligendi iusto quidem ipsum officia? Optio omnis atque velit porro, distinctio autem, quasi commodi, facilis aperiam nostrum voluptate perspiciatis.</p>
-
-          <img src="https://swiperjs.com/demos/images/nature-2.jpg" className="w-full h-full object-cover"/>
+        <SwiperSlide className="swiper-slide relative w-full h-full px-[150px]">
+          <img src="https://swiperjs.com/demos/images/nature-2.jpg" className="w-full h-full object-cover" />
+          <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 text-white text-xl font-bold">
+          Cat Slide
+        </div>
         </SwiperSlide>
-        <SwiperSlide className='swiper-slide bg-center bg-cover w-[300px] h-[300px]'>
-        <p className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center text-white text-xl font-bold" >Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis neque sint eligendi iusto quidem ipsum officia? Optio omnis atque velit porro, distinctio autem, quasi commodi, facilis aperiam nostrum voluptate perspiciatis.</p>
-
-          <img src="https://swiperjs.com/demos/images/nature-3.jpg" className="w-full h-full object-cover"/>
+        <SwiperSlide className="swiper-slide relative w-full h-full px-[150px]">
+          <img src="https://swiperjs.com/demos/images/nature-3.jpg" className="w-full h-full object-cover" />
+          <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 text-white text-xl font-bold">
+          Bird Slide
+        </div>
         </SwiperSlide>
-        <SwiperSlide className='swiper-slide bg-center bg-cover w-[300px] h-[300px]'>
-        <p className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center text-white text-xl font-bold" >Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis neque sint eligendi iusto quidem ipsum officia? Optio omnis atque velit porro, distinctio autem, quasi commodi, facilis aperiam nostrum voluptate perspiciatis.</p>
-
+        <SwiperSlide className="swiper-slide relative w-full h-full px-[150px]">
           <img src="https://swiperjs.com/demos/images/nature-4.jpg" className="w-full h-full object-cover"/>
         </SwiperSlide>
-        <SwiperSlide className='swiper-slide bg-center bg-cover w-[300px] h-[300px]'>
-        <p className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center text-white text-xl font-bold" >Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis neque sint eligendi iusto quidem ipsum officia? Optio omnis atque velit porro, distinctio autem, quasi commodi, facilis aperiam nostrum voluptate perspiciatis.</p>
-
-          <img src="https://swiperjs.com/demos/images/nature-5.jpg" className="w-full h-full object-cover"/>
-        </SwiperSlide>
-        <SwiperSlide className='swiper-slide bg-center bg-cover w-[300px] h-[300px]'>
-        <p className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center text-white text-xl font-bold" >Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis neque sint eligendi iusto quidem ipsum officia? Optio omnis atque velit porro, distinctio autem, quasi commodi, facilis aperiam nostrum voluptate perspiciatis.</p>
-
-          <img src="https://swiperjs.com/demos/images/nature-6.jpg" className="w-full h-full object-cover"/>
-          </SwiperSlide>
       </Swiper>
+      </>
   )
 }
 
