51395d620dd228cffccec254998c197461d553b3
Valeriusdev <kakao757@gmail.com>
2025-02-18 00:47:02 -0800

    implement initial database seeding

---
+++ server/dummydata/animals.js
@@ -0,0 +1,60 @@
+const animals = [
+  {
+    name: "Bella",
+    animalType: "Cat",
+    age: 1,
+    gender: "Female",
+    image: "./images/1.jpg",
+  },
+  {
+    name: "Buddy",
+    animalType: "dog",
+    age: 1,
+    gender: "Male",
+    image: "./images/2.jpg",
+  },
+  {
+    name: "Leo",
+    animalType: "cat",
+    age: 1,
+    gender: "Male",
+    image: "./images/3.jpg",
+  },
+  {
+    name: "Snowy",
+    animalType: "dog",
+    age: 1,
+    gender: "Female",
+    image: "./images/4.jpg",
+  },
+  {
+    name: "Oliver",
+    animalType: "dog",
+    age: 2,
+    gender: "Male",
+    image: "./images/5.jpg",
+  },
+  {
+    name: "Teddy",
+    animalType: "cat",
+    age: 4,
+    gender: "Male",
+    image: "./images/6.jpg",
+  },
+  {
+    name: "Sunny",
+    animalType: "hamster",
+    age: 1,
+    gender: "Male",
+    image: "./images/7.jpg",
+  },
+  {
+    name: "Daisy",
+    animalType: "hamster",
+    age: 2,
+    gender: "Female",
+    image: "./images/8.jpg",
+  },
+];
+
+export default animals;
