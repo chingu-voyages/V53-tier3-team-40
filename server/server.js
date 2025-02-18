354898be95133b492981841be51ed6755b0b59ae
Valeriusdev <kakao757@gmail.com>
2025-02-18 01:37:33 -0800

    add middleware

--- server/server.js
+++ server/server.js
@@ -1,15 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
-dotenv.config()
+dotenv.config();
 import connectDB from "./config/db.js";
+import animalRoutes from "./routes/animalRoutes.js";
 const port = process.env.PORT || 5000;
 
-connectDB()
+connectDB();
 
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("API is running ...");
 });
+
+app.use("/api/animals", animalRoutes);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
