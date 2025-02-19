a9d089758e94bfb6664bd00f5617d27959e8b96f
Tayrika <rika13tay@gmail.com>
2025-02-19 14:32:31 +0900

    Make github link appear in light mode

--- client/src/components/Footer/FooterBottom.jsx
+++ client/src/components/Footer/FooterBottom.jsx
@@ -2,11 +2,11 @@ function FooterBottom() {
   return (
     <div className="relative w-full flex sm:flex-col sm:items-center flex-row items-start justify-between dark:bg-gray-900 dark:text-white">
       <span className="flex items-center justify-center order-first text-sm text-footerBoxColor mb-4 pt-10">
-        <i className="fab fa-github text-4xl mr-4 text-white"></i>
+        <i className="fab fa-github text-4xl mr-4 text-gray-400 dark:text-white"></i>
         <a
           href="https://github.com/chingu-voyages/V53-tier3-team-40"
           target="_blank"
-          className="hover:text-white text-lg text-white">
+          className="hover:text-emerald-400 text-lg text-gray-400 dark:text-white dark:hover:text-emerald-400">
           Github Project Link - February 2025
         </a>
       </span>
